f9e0bf44910d300a8780433a296b9d7dbdff2f8d
Normandes Junior <normandesjr@gmail.com>
2018-07-15 20:12:34 -0300

    Enabling line number

--- lib/kitabu/extensions/rouge.rb
+++ lib/kitabu/extensions/rouge.rb
@@ -4,7 +4,8 @@ module Rouge
       def rouge_formatter(lexer)
         options = lexer.respond_to?(:options) ? lexer.options : {}
         extra_class = options["css_class"]
-        Formatters::HTMLLegacy.new({css_class: "highlight #{lexer.tag} #{extra_class}"}.merge(options))
+        enable_line_numbers = options["line_numbers"] ? true : false
+        Formatters::HTMLLegacy.new({css_class: "highlight #{lexer.tag} #{extra_class}", line_numbers: enable_line_numbers}.merge(options))
       end
     end
   end
